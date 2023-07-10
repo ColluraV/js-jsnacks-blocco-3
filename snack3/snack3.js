2806e0a9e5b50cf219406a38a437042baf6b47cb
ColluraV <valerio.18.02.89@hotmail.it>
2023-07-10 09:47:25 +0200

    sistemato il bonus

--- snack3/snack3.js
+++ snack3/snack3.js
@@ -3,10 +3,8 @@ const btnNumArray = document.querySelector(".confermaNum");
 const printArray = document.querySelector("[id=risultato]");
 
 
-
-
 btnNumArray.addEventListener("click", function () {
-    /*let iRandom =0;            variabile legata ciclowhile nascosto*/
+             /* variabile legata ciclowhile nascosto*/
     const numerInputElement = document.querySelector("[id=numeroInput]").value;
 
     console.log(numerInputElement);
@@ -14,20 +12,25 @@ btnNumArray.addEventListener("click", function () {
     
     for (let i=0; i<numerInputElement; i++) {
         
+        let iRandom =0;
+
         //dichiaro le variabili che saranno ripetute ed eventualmente stampate "i" volte
+        
         let randomArray=[];
         const div = document.createElement("div");            
+
+        /* in alternativa al ciclo while
 
         for (let a = 0; a < 10; a++) {
             const randomNum = Math.round(Math.random() * 100);
             randomArray.push(randomNum);
-         }     
-            // il ciclo while non funziona, domandare l'errore
-            /*while ( iRandom < 10 ){
+         } */  
+             
+            while ( iRandom < 10 ){
                 let numRandom = Math.round(Math.random() * 100 );
                 randomArray.push (numRandom);
                 iRandom+= 1;
-            }*/
+            }
          
             console.log( randomArray )
             div.innerHTML= (randomArray)
